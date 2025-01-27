6587d92ca9edc1ad878fd43e31d8381b2c298b82
jbravo2buffs <jbravo2@buffs.wtamu.edu>
2025-01-27 16:06:36 -0600

    Homework1 code file

---
+++ ProgramHW1.cs
@@ -0,0 +1,18 @@
+﻿namespace Homework1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Given values
+        double X = 2.5;
+        double Y = 3.3;
+
+        // Calculate Z using the formula Z = 4*X*X + 3Y
+        double Z = 4*X*X+3*Y;
+
+        // Print the result in the specified format
+        Console.WriteLine($"The vaule of Z when X = {X} and Y = {Y} is {Z}");
+
+    }
+}
